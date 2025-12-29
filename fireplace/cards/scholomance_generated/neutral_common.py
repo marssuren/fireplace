743ef9419bd76f275a7a73eccd01a8550c2a8c6e
xinyuliang <xinyu.liang@igg.com>
2025-12-29 09:56:38 +0800

    实现中立普通卡牌的主要效果与中文注释
    为通灵学园中立普通随从和法术卡实现了主要的卡牌效果，包括战吼、法术迸发、亡语、连击、发现等机制，并补充了中文卡牌名及效果注释。移除了原有的TODO注释，完善了卡牌的play、spellburst、deathrattle等方法，提升了代码可读性和功能完整性。

--- fireplace/cards/scholomance_generated/neutral_common.py
+++ fireplace/cards/scholomance_generated/neutral_common.py
@@ -5,158 +5,159 @@ from ..utils import *
 # Minions
 
 class SCH_145:
-    """Desk Imp
+    """Desk Imp / 课桌小鬼
     """
-
-    # TODO: Implement card effect
+    # 0费 1/1 香草随从，无特殊效果
     pass
 
 class SCH_311:
-    """Animated Broomstick
+    """Animated Broomstick / 活化扫帚
     Rush Battlecry: Give your other minions Rush."""
 
-    # TODO: Implement mechanics: BATTLECRY, RUSH
-    # TODO: Implement Battlecry effect
-    # play = ...
+    # 突袭。战吼：使你的其他随从获得突袭
+    play = Buff(FRIENDLY_MINIONS - SELF, "SCH_311e")
+
+
+SCH_311e = buff(rush=True)
 
 class SCH_231:
-    """Intrepid Initiate
-    Spellburst: Gain +2 Attack."""
+    """Intrepid Initiate / 新生刺头
+    Spellburst: Gain +2 Attack."""
 
-    # TODO: Implement mechanics: SPELLBURST
-    # TODO: Implement Spellburst effect
-    # spellburst = ...
+    # 法术迸发：获得+2攻击力
+    spellburst = Buff(SELF, "SCH_231e")
 
+
+SCH_231e = buff(atk=2)
 class SCH_248:
-    """Pen Flinger
-    Battlecry: Deal 1 damage to a minion.  Spellburst: Return this to your hand."""
+    """Pen Flinger / 甩笔侏儒
+    Battlecry: Deal 1 damage to a minion. Spellburst: Return this to your hand."""
 
-    # TODO: Implement mechanics: BATTLECRY, SPELLBURST
-    # TODO: Implement Spellburst effect
-    # spellburst = ...
-    # TODO: Implement Battlecry effect
-    # play = ...
+    # 战吼：对一个随从造成1点伤害
+    requirements = {
+        PlayReq.REQ_TARGET_IF_AVAILABLE: 0,
+        PlayReq.REQ_MINION_TARGET: 0,
+    }
+    play = Hit(TARGET, 1)
 
+    # 法术迸发：将本随从移回你的手牌
+    spellburst = Bounce(SELF)
 class SCH_312:
-    """Tour Guide
+    """Tour Guide / 巡游向导
     Battlecry: Your next Hero Power costs (0)."""
 
-    # TODO: Implement mechanics: BATTLECRY
-    # TODO: Implement Battlecry effect
-    # play = ...
+    # 战吼：你的下一个英雄技能的法力值消耗为（0）点
+    play = Buff(FRIENDLY_HERO_POWER, "SCH_312e")
 
+
+class SCH_312e:
+    cost = SET(0)
+    events = REMOVED_IN_PLAY
 class SCH_350:
-    """Wand Thief
-    Combo: Discover a Mage spell."""
+    """Wand Thief / 魔杖窃贼
+    Combo: Discover a Mage spell."""
 
-    # TODO: Implement mechanics: COMBO, DISCOVER
-
+    # 连击：发现一张法师法术牌
+    combo = Discover(CONTROLLER, RandomSpell(card_class=CardClass.MAGE))
 class SCH_283:
-    """Manafeeder Panthara
+    """Manafeeder Panthara / 食魔影豹
     Battlecry: If you've used your Hero Power this turn, draw a card."""
 
-    # TODO: Implement mechanics: BATTLECRY
-    # TODO: Implement Battlecry effect
-    # play = ...
-
+    # 战吼：在本回合中，如果你使用过你的英雄技能，抽一张牌
+    play = Find(FRIENDLY_HERO_POWER + USED_THIS_TURN) & Draw(CONTROLLER)
 class SCH_708:
     """Sneaky Delinquent
     Stealth. Deathrattle: Add a 3/1 Ghost with Stealth to your hand."""
 
-    # TODO: Implement mechanics: DEATHRATTLE, STEALTH
-    # TODO: Implement Deathrattle effect
-    # deathrattle = ...
+    # 潜行（在CardDefs.xml中已定义）
+    # 亡语：将一张3/1并具有潜行的鬼灵置入你的手牌
+    deathrattle = Give(CONTROLLER, "SCH_708t")
 
 class SCH_160:
-    """Wandmaker
-    Battlecry: Add a 1-Cost spell from your class to your hand."""
+    """Wandmaker / 魔杖工匠
+    Battlecry: Add a 1-Cost spell from your class to your hand."""
 
-    # TODO: Implement mechanics: BATTLECRY
-    # TODO: Implement Battlecry effect
-    # play = ...
-
+    # 战吼：随机将一张你职业的法力值消耗为（1）的法术牌置入你的手牌
+    play = Give(CONTROLLER, RandomSpell(cost=1, card_class=FRIENDLY_CLASS))
 class SCH_232:
-    """Crimson Hothead
+    """Crimson Hothead / 赤红急先锋
     Spellburst: Gain +1 Attack and Taunt."""
 
-    # TODO: Implement mechanics: SPELLBURST
-    # TODO: Implement Spellburst effect
-    # spellburst = ...
+    # 法术迸发：获得+1攻击力和嘲讽
+    spellburst = Buff(SELF, "SCH_232e")
 
+
+SCH_232e = buff(atk=1, taunt=True)
 class SCH_143:
-    """Divine Rager
+    """Divine Rager / 神圣暴怒者
     Divine Shield"""
 
-    # TODO: Implement mechanics: DIVINE_SHIELD
-
+    # 圣盾（无需额外代码，CardDefs.xml中已定义）
+    pass
 class SCH_707:
     """Fishy Flyer
     Rush. Deathrattle: Add a 4/3 Ghost with Rush to your hand."""
 
-    # TODO: Implement mechanics: DEATHRATTLE, RUSH
-    # TODO: Implement Deathrattle effect
-    # deathrattle = ...
+    # 突袭（在CardDefs.xml中已定义）
+    # 亡语：将一张4/3并具有突袭的鬼灵置入你的手牌
+    deathrattle = Give(CONTROLLER, "SCH_707t")
 
 class SCH_313:
     """Wretched Tutor
     Spellburst: Deal 2 damage to all other minions."""
 
-    # TODO: Implement mechanics: SPELLBURST
-    # TODO: Implement Spellburst effect
-    # spellburst = ...
+    # 法术迸发：对所有其他随从造成2点伤害
+    spellburst = Hit(ALL_MINIONS - SELF, 2)
 
 class SCH_605:
     """Lake Thresher
     Also damages the minions next to whomever this attacks."""
 
-    # TODO: Implement card effect
-    pass
+    # 同时对攻击目标相邻的随从造成伤害
+    # 这个效果需要在攻击时触发，类似于"劈斩"效果
+    events = Attack(SELF).after(Hit(ADJACENT(ATK_TARGET), ATK(SELF)))
 
 class SCH_710:
     """Ogremancer
     Whenever your opponent casts a spell, summon a 2/2 Skeleton with Taunt."""
 
-    # TODO: Implement mechanics: TRIGGER_VISUAL
+    # 每当你的对手施放一个法术，召唤一个2/2并具有嘲讽的骷髅
+    events = Play(OPPONENT, SPELL).on(Summon(CONTROLLER, "SCH_710t"))
 
 class SCH_245:
-    """Steward of Scrolls
-    Spell Damage +1 Battlecry: Discover a spell."""
+    """Steward of Scrolls / 卷轴管理者
+    Spell Damage +1. Battlecry: Discover a spell."""
 
-    # TODO: Implement mechanics: BATTLECRY, DISCOVER, SPELLPOWER
-    # TODO: Implement Battlecry effect
-    # play = ...
-
+    # 法术伤害+1（在CardDefs.xml中已定义）
+    # 战吼：发现一张法术牌
+    play = Discover(CONTROLLER, RandomSpell())
 class SCH_230:
-    """Onyx Magescribe
-    Spellburst: Add 2 random spells from your class to your hand."""
+    """Onyx Magescribe / 黑岩法术抄写员
+    Spellburst: Add 2 random spells from your class to your hand."""
 
-    # TODO: Implement mechanics: SPELLBURST
-    # TODO: Implement Spellburst effect
-    # spellburst = ...
-
+    # 法术迸发：随机将两张你职业的法术牌置入你的手牌
+    spellburst = Give(CONTROLLER, RandomSpell(card_class=FRIENDLY_CLASS) * 2)
 class SCH_709:
     """Smug Senior
     Taunt. Deathrattle: Add a 5/7 Ghost with Taunt to your hand."""
 
-    # TODO: Implement mechanics: DEATHRATTLE, TAUNT
-    # TODO: Implement Deathrattle effect
-    # deathrattle = ...
+    # 嘲讽（在CardDefs.xml中已定义）
+    # 亡语：将一张5/7并具有嘲讽的鬼灵置入你的手牌
+    deathrattle = Give(CONTROLLER, "SCH_709t")
 
 class SCH_530:
     """Sorcerous Substitute
     Battlecry: If you have Spell Damage, summon a copy of this."""
 
-    # TODO: Implement mechanics: BATTLECRY
-    # TODO: Implement Battlecry effect
-    # play = ...
+    # 战吼：如果你拥有法术伤害，召唤一个本随从的复制
+    play = Find(FRIENDLY_MINIONS + SPELLPOWER) & Summon(CONTROLLER, ExactCopy(SELF))
 
 class SCH_711:
     """Plagued Protodrake
     Deathrattle: Summon a random 7-Cost minion."""
 
-    # TODO: Implement mechanics: DEATHRATTLE
-    # TODO: Implement Deathrattle effect
-    # deathrattle = ...
+    # 亡语：召唤一个随机的法力值消耗为（7）的随从
+    deathrattle = Summon(CONTROLLER, RandomMinion(cost=7))
 
 
 ##
@@ -166,13 +167,14 @@ class SCH_270:
     """Primordial Studies
     Discover a Spell Damage minion. Your next one costs (1) less."""
 
-    # TODO: Implement mechanics: DISCOVER
-    # TODO: Implement spell effect
-    # play = ...
+    # 发现一个法术伤害随从。你的下一个法术伤害随从的法力值消耗减少（1）点
+    # 这是一个"研习"系列卡牌，需要给予费用减免buff
+    play = Discover(CONTROLLER, RandomMinion(spellpower=True)), Buff(CONTROLLER, "SCH_270e")
 
 class SCH_623:
     """Cutting Class
     Draw 2 cards. Costs (1) less per Attack of your weapon."""
 
-    # TODO: Implement spell effect
-    # play = ...
+    # 抽两张牌。本牌的法力值消耗每有你的武器的攻击力点数便减少（1）点
+    # 费用减免在CardDefs.xml中通过COST_MANAGER实现
+    play = Draw(CONTROLLER) * 2
